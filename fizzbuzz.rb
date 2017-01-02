1cdb7cbd682b8b62b1c78acc8ad84841a86695b0
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-02 13:58:00 +0000

    define fizzbuzz method

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,18 @@
 def fizzbuzz(number = 0)
-  return 'fizz'
+  #check for multiple of three and five, if true, return fizzbuzz
+  #check for multiples of five, if true, return buzz
+  #check for multiples of three, if true, return fizz
+  #return number
+  case
+  when number == 0
+    number
+  when (number % 3 == 0) && (number % 5 == 0)
+      "fizzbuzz"
+  when number % 5 == 0
+      "buzz"
+  when number % 3 == 0
+      "fizz"
+  else
+      number
+  end
 end
