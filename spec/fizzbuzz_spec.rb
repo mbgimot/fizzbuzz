957f69bf8ca13d17df1512d5912afdf0afb76df0
Mitchell Goldbay <mitchellgoldbay@gmail.com>
2017-01-02 14:11:54 +0000

    Added test cases from exercise for validation

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -51,3 +51,13 @@ describe 'everyting_else' do
     expect(fizzbuzz(-1)).to eq -1
   end
 end
+
+describe 'exercise cases' do
+  inputs = (1..20).to_a
+  outputs = [1, 2, 'fizz', 4, 'buzz', 'fizz', 7, 8, 'fizz', 'buzz', 11, 'fizz', 13, 14, 'fizzbuzz', 16, 17, 'fizz', 19, 'buzz']
+  inputs.each do |index|
+    it "The input #{index} should output #{outputs[index-1]}" do
+      expect(fizzbuzz(index)).to eq outputs[index-1]
+    end
+  end
+end
